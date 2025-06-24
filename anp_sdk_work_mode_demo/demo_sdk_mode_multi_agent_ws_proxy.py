247a5b42fb0516f0bc1dacc1d356df981c8afe75
seanzhang9999 <seanzhang9999@gmail.com>
2025-06-24 20:51:52 +0800

    修正为open_sdk预发布结构

--- anp_sdk_work_mode_demo/demo_sdk_mode_multi_agent_ws_proxy.py
+++
@@ -1,9 +0,0 @@
-from anp_open_sdk.anp_sdk import ANPSDK
-from anp_open_sdk.anp_sdk_agent import LocalAgent
-from anp_open_sdk.sdk_mode import SdkMode
-
-if __name__ == "__main__":
-    agent1 = LocalAgent(...)
-    agent2 = LocalAgent(...)
-    sdk = ANPSDK(mode=SdkMode.SDK_WS_PROXY_SERVER, agents=[agent1, agent2], ws_host="0.0.0.0", ws_port=9527)
-    sdk.start_server()
--- test/readme.md
+++
@@ -1,6 +0,0 @@
-# 测试方法
-
-## unified_config.py测试
-```
-python anp_open_sdk/test/test_unified_config.py
-```
